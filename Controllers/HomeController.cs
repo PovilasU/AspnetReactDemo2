c528920bd8d357b1960da82684b17cacc84d2f1b
povilasurbonas <povilas.urbonas85@gmail.com>
2020-09-24 17:05:29 +0200

    Comment box is done and  working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -52,6 +52,16 @@ namespace ReactDemo2.Controllers
             return Json(_comments);
         }
 
+        [Route("comments/new")]
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            // Create a fake ID for this comment
+            comment.Id = _comments.Count + 1;
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+
 
 
     }
