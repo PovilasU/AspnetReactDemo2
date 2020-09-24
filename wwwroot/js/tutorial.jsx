69177a960f00118db050b5c68df11b6e2d354a3f
povilasurbonas <povilas.urbonas85@gmail.com>
2020-09-24 14:12:06 +0200

    adding react components with props

--- wwwroot/js/tutorial.jsx
+++ wwwroot/js/tutorial.jsx
@@ -1,9 +1,43 @@
-﻿class CommentBox extends React.Component {
-    render() {
-        return (
-            <div className="commentBox">Hello, world! I am a CommentBox!!!224.</div>
-        );
-    }
+﻿class Comment extends React.Component {
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">{this.props.author}</h2>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+class CommentList extends React.Component {
+  render() {
+    return (
+      <div className="commentList">
+        <Comment author="Daniel adad">Hello ReactJS.NET World!</Comment>
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+}
+
+class CommentForm extends React.Component {
+  render() {
+    return <div className="commentForm">Hello, world! I am a CommentForm.</div>;
+  }
+}
+
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <div className="commentBox">
+        {' '}
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<CommentBox />, document.getElementById('content'));
