a8a51201e4ea7f12d252c1b964f0282d4e2d6d1a
povilasurbonas <povilas.urbonas85@gmail.com>
2020-09-24 14:58:37 +0200

    updated homeController with comments, now comments json can be feth through url /comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReactDemo2.Models;
+
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,46 @@ namespace ReactDemo2.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        public ActionResult Index()
         {
             return View();
         }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
+
+
     }
 }
--- Models/CommentModel.cs
+++ Models/CommentModel.cs
@@ -7,5 +7,8 @@ namespace ReactDemo2.Models
 {
     public class CommentModel
     {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
     }
 }
